08885b36f9ea0538271d308326baf905cc09ec8c
fhgjbb <136576639+fhgjbb@users.noreply.github.com>
2025-05-16 17:19:33 +0800

    initial commit

--- connect to github.R
+++ connect to github.R
@@ -1,5 +1,21 @@
 git remote add origin https://github.com/fhgjbb/hoomie.github.io.git
-git branch -M main
-git add --all
-git commit -m "initial commit"
-git push -u origin main
+git remote add origin <URL> - 添加遠端
+git branch -M main - 重命名主分支
+git add --all - 添加所有變更
+git commit -m "<訊息>" - 提交變更
+git push -u origin main - 推送到遠端
+#
+# 總結：設定遠端倉庫，重命名主分支，添加並提交所有更改，最後推送代碼到 GitHub。
+# 注意：這是將本地代碼同步到遠端倉庫的常見步驟。
+
+ count: 63 - 物件數
+ size: 531 KB - 儲存庫大小 (鬆散)
+ in-pack: 0 - 無打包
+ packs: 0 - 無打包檔
+ size-pack: 0B - 打包大小
+ prune-packable: 0 - 無可打包
+ garbage: 0 - 無垃圾
+ size-garbage: 0B - 垃圾大小
+
+ 總結：儲存庫很小 (< 1MB)，遠小於 GitHub Pages 限制 (1GB)。
+ 注意：此為 .git 大小，非網站最終大小。
